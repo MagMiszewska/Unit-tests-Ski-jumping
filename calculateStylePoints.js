ccf9a745c8ab6579a09f8dc17c4f2502ad9bb282
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-08-16 11:55:01 +0200

    Update function for calculate style

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,7 +1,15 @@
 const calculateStylePoints = (styleNotes) => {
-    const maxNote = Math.max(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
-    const minNote = Math.min(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
-    return styleNotes[0] + styleNotes[1] + styleNotes[2] + styleNotes[3] + styleNotes[4] - maxNote - minNote;
+    const maxNote = Math.max(...styleNotes);
+    const minNote = Math.min(...styleNotes);
+    return styleNotes.reduce((a, b) => a + b) - (maxNote + minNote);
 };
 
 module.exports = calculateStylePoints;
+
+// const calculateStylePoints = (styleNotes) => {
+//     const maxNote = Math.max(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
+//     const minNote = Math.min(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
+//     return styleNotes[0] + styleNotes[1] + styleNotes[2] + styleNotes[3] + styleNotes[4] - maxNote - minNote;
+// };
+
+// module.exports = calculateStylePoints;
